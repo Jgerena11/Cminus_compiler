bc3b83c35161a27ead73c4e976a3c8e234c031de
Jose <jgerena1111@gmail.com>
2020-02-19 01:11:14 -0500

    added functions to parser class

--- project2/parser.py
+++ project2/parser.py
@@ -6,6 +6,7 @@ f = open(file, "r")
 line = f.readline()
 o = open('output.txt', 'w')
 
+tokens = []
 # --------compiled patterns---------
 open_comment = re.compile(r'/\*|//')
 close_comment = re.compile(r'\*/')
@@ -19,7 +20,8 @@ kw = re.compile(r'\belse\b|\bif\b|\breturn\b|\bvoid\b|\bwhile\b|\bint\b')
 def error(text, i):
     error = text[i]
     i+=1
-    o.write('ERROR: ' + error + '\n')
+    o.write('ERROR' + error)
+    tokens.append('ERROR')
     return i
 
 #process comments
@@ -49,10 +51,12 @@ def special_symbols(text, i):
                 return comments(text[i+2:len(text)], i, text[i:j+1])
             if compound_special_symbols.match(text[i:j+1]):
                 o.write(text[i:j+1]+'\n')
+                tokens.append(text[i:j+1])
                 i += 2
                 j += 1
             elif special_sym.match(text[i]):
                 o.write(text[i]+'\n')
+                tokens.append(text[i])
                 i += 1
                 j += 1
             elif not special_sym.match(text[i]):
@@ -60,6 +64,7 @@ def special_symbols(text, i):
         else:
             if special_sym.match(text[i]):
                 o.write(text[i]+'\n')
+                tokens.append(text[i])
             elif not special_sym.match(text[i]):
                 return error(text,i)
             i += 1
@@ -72,13 +77,17 @@ def letters(text, i):
         word += text[i]
         i += 1
     if text[i] == '_':
-        if words.match(word): o.write('ID: ' + word + '\n')
+        if words.match(word):
+            o.write('ID: ' + word + '\n')
+            tokens.append('ID')
         return error(text, i)
     if kw.match(word):
-        o.write(word + '\n')
+        o.write('KW' + word + '\n')
+        tokens.append('KW')
         return i
     else:
         o.write('ID: ' + word + '\n')
+        tokens.append('ID')
     return i
 
 #process numbers
@@ -88,6 +97,7 @@ def numbers(text, i):
         NUM += text[i]
         i += 1
     o.write('NUM: ' + NUM + '\n')
+    tokens.append('NUM')
     return i
 
 while line:
@@ -110,5 +120,69 @@ while line:
         else:
             i += 1
     line = f.readline()
+tokens.append('$')
 f.close()
 o.close()
+
+class Parser:
+    count = 0
+    result = ""
+    def __init__(self, tokens):
+        self.tokens = tokens
+
+    def program(self):
+        if self.declaration_list() and self.tokens[self.count] == '$':
+            return True
+        else:
+            return False
+
+    def declaration_list(self):
+        if self.declaration():
+            if self.declaration_list_prime():
+                return True
+            else:
+                return False
+
+    def match(self,token, t):
+        if token == t:
+            self.count += 1
+            return True
+
+    def declaration_list_prime(self):
+        if self.declaration():
+            if self.declaration_list_prime():
+                return True
+        else:
+            return False
+
+    def type_specifier(self):
+        if self.tokens[self.count] == 'int':
+            if self.match(tokens[self.count], 'int'):
+                return True
+        elif self.tokens[self.count] == 'void':
+            if self.match(tokens[self.count], 'void'):
+                return True
+        else:
+            return False
+
+    def declaration(self):
+        if self.type_specifier():
+            if self.tokens[self.count] == 'ID':
+                if self.match(self.tokens[self.count], 'ID'):
+                    if self.declaration_prime:
+                        return True
+            else:
+                return False
+        else:
+            return False
+
+    def declaration_prime(self):
+        if self.var_declaration:
+            return True
+        elif self.tokens[self.count] == '(':
+            if self.match(self.tokens[self.count], '('):
+                if self.params():
+                    if self.tokens[self.count] == ')':
+                        if self.match(self.tokens[self.count], ')'):
+                            if self.compound_stmt():
+                                return True
