37eb7e593131deb5fd35583888ae6452c3227339
Jose <jgerena1111@gmail.com>
2020-03-20 01:25:40 -0400

    added sem analysis classes and reconfigured scope class

--- project4/prj4.py
+++ project4/prj4.py
@@ -2,9 +2,107 @@ from scanner import *
 from sem_analyzer import *
 import sys
 
-class Parser:
+class Program:
+    declaration = None
+    PS = ProgramScope()
 
-    SA = SemanticAnalyzer()
+    def declare(self):
+        self. declaration = Declaration()
+        return self.declaration
+
+class Declaration(Program):
+    pass
+    type = None
+    id = None
+    dec_prime = None
+
+    def init_dec_prime(self):
+        self.dec_prime = DeclarationPrime(self.type, self.id)
+        return self.dec_prime
+
+    def verify(self, ):
+        if self.dec_prime.verify():
+            return True
+        else:
+            print('failed in declaration')
+            return False
+
+    def assign_type(self, type):
+        self.type = type
+
+    def assign_id(self, id):
+        if self.PS.current_scope_has_id(id):
+            print(self.type)
+            return False
+        else:
+            self.id = id
+            return True
+
+class TypeSpecifier:
+    def __init__(self, type):
+        self.type = type
+
+class DeclarationPrime(Declaration):
+    pass
+    var_declaration = None
+    func_declaration = None
+
+    def __init__(self, type, id):
+        self.id = id
+        self.type = type
+
+    def verify(self):
+
+        if self.var_declaration is None:
+            print('failed in declaration prime1')
+            return False
+        elif self.func_declaration is None:
+            if self.var_declaration.verify():
+                return True
+            else:
+                print('failed in declaration prime2')
+                return False
+
+    def init_var(self):
+        self.var_declaration = VarDeclaration(self.type, self.id)
+        return self.var_declaration
+
+    def init_function(self):
+        self.func_declaration = FunDeclaration()
+        return self.func_declaration
+
+class FunDeclaration:
+    type = None
+    id = None
+    params = None
+    scope = None
+    return_type = None
+    return_value = None
+
+    def __init__(self, type, id):
+        self.type = type
+        self.id = id
+        self.current_table.append(id, self)
+
+class VarDeclaration(DeclarationPrime):
+    pass
+    has_array = False
+    array = None
+    array_size = None
+
+    def __init__(self, type, id):
+        self.type = type
+        self.id = id
+
+    def verify(self):
+
+        if self.type == 'void':
+            print('failed in var_declaration')
+            return False
+        self.PS.add(self.id, self)
+        return True
+
+class Parser:
 
     var_declaration_first = [';', '[']
     type_specifier_first = ['int', 'void']
@@ -25,50 +123,55 @@ class Parser:
         self.current_token = tokens[self.count]
 
     def program(self):
-        self.declaration_list()
+        program = Program()
+        self.declaration_list(program)
         if self.current_token.type == '$':
             return
         else:
             self.result = False
 
-    def declaration_list(self):
-        self.declaration()
-        self.declaration_list_prime()
+    def declaration_list(self, program):
+        self.declaration(program)
+        self.declaration_list_prime(program)
 
     def accept(self, token):
-        list.tail.append(token.value, token.type)
         self.count += 1
         self.current_token = self.tokens[self.count]
 
-    def declaration_list_prime(self):
+    def declaration_list_prime(self, program):
         if self.current_token.type in ['int', 'void']:
             self.declaration()
             self.declaration_list_prime()
 
-    def type_specifier(self, declaration):
+    def type_specifier(self):
         if self.current_token.type in ['int', 'void']:
-            declaration.assign_type(self.current_token.value)
+            type_spec = TypeSpecifier(self.current_token.type)
             self.accept(self.current_token)
+            return type_spec.type
         else:
             self.result = False
 
-    def declaration(self):
-        declaration = self.SA.Declaration()
-        self.type_specifier(declaration)
+    def declaration(self, program):
+        declaration = program.declare()
+        declaration.assign_type(self.type_specifier())
         if self.current_token.type =='ID':
             self.result = declaration.assign_id(self.current_token.value)
             self.accept('ID')
             self.declaration_prime(declaration)
+            self.result = declaration.verify()
         else:
             self.result = False
 
-    def var_declaration(self, declaration):
+    def var_declaration(self, declaration_prime):
+        var_dec = declaration_prime.init_var()
         if self.current_token.type == ';':
             self.accept(';')
+            self.result = False if var_dec.verify else True
             return
         elif self.current_token.type == '[':
             self.accept('[')
             if self.current_token.type == 'NUM':
+                var_dec.array_size = self.current_token.value
                 self.accept('NUM')
                 if self.current_token.type == ']':
                     self.accept(']')
@@ -104,8 +207,9 @@ class Parser:
             self.result = False
 
     def declaration_prime(self, declaration):
+        dec_prime = declaration.init_dec_prime()
         if self.current_token.type in self.var_declaration_first:
-            self.var_declaration()
+            self.var_declaration(dec_prime)
         elif self.current_token.type == '(':
             self.accept('(')
             self.params()
@@ -219,7 +323,6 @@ class Parser:
                 self.result = False
         else:
             self.result = False
-
 
     def expression_stmt(self):
         if self.current_token.type in self.expression_first:
@@ -362,7 +465,8 @@ class Parser:
             self.arg_list()
 
 try:
-    file = sys.argv[1]
+    # file = sys.argv[1]
+    file = input()
     f = open(file, 'r')
     scanner = Scanner(f)
     scanner.run_scanner()
--- project4/sem_analyzer.py
+++ project4/sem_analyzer.py
@@ -1,35 +1,6 @@
-class DoubleLinkedList:
-    class Node:
-        def __init__(self, prev, next, table):
-            self.prev = prev
-            self.next = next
-            self.table = table
-
-    def __init__(self):
-        self.head = self.Node(None, None, None)
-        self.tail = self.Node(None, None, None)
-
-    def add_last(self, data):
-        if self.head.next == None and self.tail.prev == None:
-            node = self.Node(self.head, self.tail, data)
-            self.head.next = node
-            self.tail.prev = node
-        else:
-            node = self.Node(self.tail.prev, self.tail, data)
-            self.tail.prev.next = node
-            self.tail.prev = node
-
-    def remove(self):
-        if self.tail.prev == self.head.next:
-            self.head.next = self.tail
-            self.tail.prev = self.head
-        else:
-            self.tail.prev = self.tail.prev.prev
-            self.tail.prev.next = self.tail
-
-class SemanticAnalyzer:
-    list = DoubleLinkedList
-    current_table = None
+class ProgramScope:
+    list = None
+    current_scope = None
 
     class SymbolTable:
         table = {}
@@ -43,51 +14,102 @@ class SemanticAnalyzer:
         def get_type(self, id):
             return self.table[id]
 
-    def get_type(self, id):
-        node = list.tail
-        while node is not list.head:
-            if node.table[id]:
-                return node.table.get_type
+        def get(self, id):
+            return self.table[id]
+
+        def has_id(self, id):
+            if id in self.table:
+                return True
+            else:
+                return False
+
+    class DoubleLinkedList:
+        head = None
+        tail = None
+
+        class Node:
+            prev = None
+            next = None
+
+            def __init__(self, prev, next, data):
+                self.prev = prev
+                self.next = next
+                self.scope = data
+
+        def __init__(self):
+            self.head = self.Node(None, None, None)
+            self.tail = self.Node(None, None, None)
+
+        def add_last(self, new_data):
+            if self.head.next is None and self.tail.prev is None:
+                node = self.Node(self.head, self.tail, new_data)
+                self.head.next = node
+                self.tail.prev = node
+            else:
+                node = self.Node(self.tail.prev, self.tail, new_data)
+                self.tail.prev.next = node
+                self.tail.prev = node
+
+        def remove(self):
+            if self.tail.prev == self.head.next:
+                self.head.next = self.tail
+                self.tail.prev = self.head
+            else:
+                self.tail.prev = self.tail.prev.prev
+                self.tail.prev.next = self.tail
+
+        def last(self):
+            return self.tail.prev
+
+    def __init__(self):
+        self.list = self.DoubleLinkedList()
+        self.current_scope = self.SymbolTable()
+        self.list.add_last(self.current_scope)
+
+    #----add new scope to program -----------
+    def new_scope(self):
+        table = self.SymbolTable()
+        self.current_scope = table
+        self.list.add_last(table)
+
+    def get_scope(self, id):
+        node = self.list.last()
+        while node is not self.list.head:
+            if node.table.has_id(id):
+                return node.table
             else:
                 node = node.prev
         return None
 
-    def __init__(self):
-        self.current_table = self.SymbolTable()
-        self.list.add_last(self.current_table)
+    #----- check if current scope has id --------
+    def current_scope_has_id(self, id):
+        if self.current_scope.has_id(id):
+            return True
+        else:
+            return False
 
-    def new_scope(self):
-        table = self.SymbolTable()
-        self.current_table = table
-        self.list.add_last(table)
-        
-    class Declaration:
-        type = None
-        id = None
-        declaration_prime = None
+    # check for id is reachable
+    def has_id(self, id):
+        scope = self.get_scope(id)
+        if scope.has_id(id):
+            return True
+        else:
+            return False
 
-        def assign_type(self, type):
-            self.type = type
+    #returns the type object of the id
+    def get(self, id):
+        scope = self.get_scope(id)
+        return scope.get(id)
 
-        def assign_id(self, id):
-            if self.table[id]:
-                return False
-            else:
-                self.current_table.append(self.type, id)
-                return True
+    #update type of id in the program
+    def update(self, id, data):
+        scope = self.get_scope(id)
+        scope.update(id, data)
 
-        def get_type(self, id):
-            if self.current_table
+    def add(self, id, type):
+        self.current_scope.append(id, type)
 
-    class DeclarationPrime:
-        var_declaration = None
-        params = None
 
-    class VarDeclaration:
-        value = None
-
-        def assign_value(self, value):
-            if value.type = super().type:
 
 
 
